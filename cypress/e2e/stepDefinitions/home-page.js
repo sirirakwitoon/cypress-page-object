77b492de065407bbe450da2cd24123ca0147a12b
Sirirak Witoon <sirirakwitoon@Siriraks-MacBook-Air.local>
2022-11-23 17:01:08 +0700

    add feedback page feature

--- cypress/e2e/stepDefinitions/home-page.js
+++ cypress/e2e/stepDefinitions/home-page.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import FeedbackPage from "../../../e2e/support/feedback";
 import HomePage from "../../support/home-page";
 import OnlineBankingPage from "../../support/online-banking";
 
 let homePage = new HomePage();
 let onlineBankingPage = new OnlineBankingPage();
+let feedbackPage = new FeedbackPage()
 
 Given("User open website", () => {
     homePage.expectCorrectUrl();
@@ -17,10 +19,11 @@ When("User select online banking menu", () => {
     onlineBankingPage.expectCorrectUrl()
 })
 Then("The system able to show online banking page", () => {
-
+    onlineBankingPage.expectContainPageTitle()
 })
 When("User select feedback menu", () => {
-
+     homePage.chooseFeedback()
+     feedbackPage.expectCorrectUrl()
 })
 Then("The system able to show feedback page", () => {
 
